976cb2bd8546ac666a1778ef5af8ef7c6cc52d6e
tanducCS <hotanduc.cs@gmail.com>
2023-06-16 10:12:57 +0700

    update CSS & eval method in js

--- script.js
+++ script.js
@@ -1,12 +1,40 @@
 output = document.getElementById('calculator--output');
 
+
+
 function display(param){
-    output.value = "";
     output.value += param;
 }
 
-button1 = document.getElementById("one");
-button1.onclick = function() {
-    display(button1.value);
-}
+buttons = document.querySelectorAll('.calculator__input--btn');
+console.log(buttons);
+
+let result = "";
+
+buttons.forEach(button => {
+    
+    
+    button.addEventListener('click', () => {
+        if(button.value === "="){
+            console.log(result);
+            output.value = eval(result);
+            result = output.value;
+        }
+        else if(button.value === "x"){
+                result += "*";
+                output.value = result;
+        }
+        else{
+            result += button.value;    
+            output.value = result;
+        }
+        
+    })
+    
+
+    
+});
+
+
+
 
